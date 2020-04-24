f2a410c1a67dc5d338148ed00e9714065153f414
Roy Hsu <roy.hsu@tinyworld.cc>
2020-04-24 14:48:30 +0800

    add more rich info about logs.

--- Sources/TinyConsole/LogDetail.swift
+++ Sources/TinyConsole/LogDetail.swift
@@ -11,20 +11,30 @@ import TreeCore
 import TreeUI
 
 struct LogDetail: View {
+  private let log: Log
   @ObservedObject
   var subtree: SubtreeStore<Atomic>
   
   var body: some View {
     List {
-      Subtree(
-        subtree,
-        content: { subtree in
-          SubtreeProperty(subtree)
-        },
-        leaf: { leaf in
-          LeafProperty(leaf)
-        }
-      )
+      Section(header: Text("Information")) {
+        Text("Source: \(log.source)")
+        Text("Level: \(log.level.rawValue)")
+        Text("File: \(log.file)")
+        Text("Function: \(log.function)")
+        Text("Line: \(log.line)")
+      }
+      Section(header: Text("Metadata")) {
+        Subtree(
+          subtree,
+          content: { subtree in
+            SubtreeProperty(subtree)
+          },
+          leaf: { leaf in
+            LeafProperty(leaf)
+          }
+        )
+      }
     }
   }
 }
@@ -32,6 +42,7 @@ struct LogDetail: View {
 extension LogDetail {
   init(log: Log) {
     self.init(
+      log: log,
       subtree:SubtreeStore(
         value: .string("metadata"),
         children: Primitive(metadata: log.metadata)
